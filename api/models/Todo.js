469ed1d8a3547a771ae8cdcb3923f0e45d787ae8
Erik <erocket7@outlook.com>
2024-02-22 16:50:41 -0500

    Almost done

---
+++ api/models/Todo.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+    text: {
+        type: String, 
+        required: true
+    },
+    complete: {
+        type: Boolean,
+        default: false
+    },
+    timestamp:{
+        type: String,
+        defualt: Date.now()
+    },
+
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+module.exports = Todo;
